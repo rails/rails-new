2b51f63663b37c82dbd794753c87296ff806cc0d
Rafael Mendonça França <rafael@rubyonrails.org>
2024-03-27 22:38:18 +0000

    Compile OS specific code in a separate module based on the OS

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,30 @@ use clap::Parser;
 
 use crate::docker_client::DockerClient;
 
+#[cfg_attr(unix, path = "unix.rs")]
+#[cfg_attr(windows, path = "windows.rs")]
+mod os_specific;
+
 fn main() {
     let cli = Cli::parse();
-
-    let dockerfile: &[u8];
-    let user_id: Option<u32>;
-    let group_id: Option<u32>;
-
-    if cfg!(windows) {
-        user_id = None;
-        group_id = None;
-        dockerfile = include_bytes!("../Dockerfile.windows");
-    } else {
-        user_id = Some(users::get_current_uid());
-        group_id = Some(users::get_current_gid());
-        dockerfile = include_bytes!("../Dockerfile");
-    }
 
     let ruby_version = cli.ruby_version;
     let rails_version = cli.rails_version;
 
     // Run docker build --build-arg RUBY_VERSION=$RUBY_VERSION --build-arg RAILS_VERSION=$RAILS_VERSION -t rails-new-$RUBY_VERSION-$RAILS_VERSION
     // passing the content of DOCKERFILE to the command stdin
-    let mut child = DockerClient::build_image(&ruby_version, &rails_version, user_id, group_id)
-        .spawn()
-        .expect("Failed to execute process");
+    let mut child = DockerClient::build_image(
+        &ruby_version,
+        &rails_version,
+        os_specific::get_user_id(),
+        os_specific::get_group_id(),
+    )
+    .spawn()
+    .expect("Failed to execute process");
 
     let mut stdin = child.stdin.take().expect("Failed to open stdin");
     std::thread::spawn(move || {
-        stdin.write_all(dockerfile).unwrap();
+        stdin.write_all(os_specific::dockerfile_content()).unwrap();
     });
 
     let status = child.wait().expect("failed to wait on child");
