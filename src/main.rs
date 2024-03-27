8901a7503bd9a2bc9d6002938e2292b366468898
Rafael Mendonça França <rafael@rubyonrails.org>
2024-03-27 22:08:23 +0000

    Generate the Rails application with the same user as the caller
    Read the user id and group id of the caller and pass them to the Docker
    so the generate folders are with the same user as the caller.
    
    Fixes #4.

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,26 @@ use crate::docker_client::DockerClient;
 fn main() {
     let cli = Cli::parse();
 
-    // read the content of the DOCKERFILE and store it in a variable
-    let dockerfile = include_bytes!("../Dockerfile");
+    let dockerfile: &[u8];
+    let user_id: Option<u32>;
+    let group_id: Option<u32>;
+
+    if cfg!(windows) {
+        user_id = None;
+        group_id = None;
+        dockerfile = include_bytes!("../Dockerfile.windows");
+    } else {
+        user_id = Some(users::get_current_uid());
+        group_id = Some(users::get_current_gid());
+        dockerfile = include_bytes!("../Dockerfile");
+    }
 
     let ruby_version = cli.ruby_version;
     let rails_version = cli.rails_version;
 
     // Run docker build --build-arg RUBY_VERSION=$RUBY_VERSION --build-arg RAILS_VERSION=$RAILS_VERSION -t rails-new-$RUBY_VERSION-$RAILS_VERSION
     // passing the content of DOCKERFILE to the command stdin
-    let mut child = DockerClient::build_image(&ruby_version, &rails_version)
+    let mut child = DockerClient::build_image(&ruby_version, &rails_version, user_id, group_id)
         .spawn()
         .expect("Failed to execute process");
 
